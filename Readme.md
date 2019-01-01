cd95f8f1126d011092810ebd633de2a1b258930f
Ben Lippmeier <benl@ouroborus.net>
2019-01-01 22:33:01 +1100

    Get extension working

--- Readme.md
+++ Readme.md
@@ -1,11 +1,9 @@
 # Salt Visual Studio Code language extension
 
-Visual Studio Code language extension for the Salt language.
-
 So far it only has the syntax definition.
 
 ### Building
-Use VSCode for development of the extension itself. Load the code into a new workspace, select the `src/extension.ts` module and run the `Start Debugging` command in VSCode. This should open a new VSCode window with the current version of the extension running. After modifing some of the code, use the `Reload` command in the VSCode debug pane to reload the extension into the second window.
+Use VSCode for development of the extension itself. Load the code into a new workspace, select the `src/extension.ts` module and run the `Start Debugging` command in VSCode. This should open a new VSCode window with the current version of the extension running. After modifing some of the code, use the `Reload` command in the VSCode debug pane to reload the extension in the second window.
 
 ### Packaging
 Once you're happy with the changes, package the extension into a `.vsix` file ready to installed more permanently into VSCode. Use the following commands from the root of the extension source directory. The `vsce` package is the VSCode Extension management command line tool
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+
+import * as vscode   from 'vscode';
+import * as vsclient from 'vscode-languageclient';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "salt-vscode" is now active!');
+	console.log('Salt extension activating');
 
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
+	let dHello = vscode.commands.registerCommand('extension.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
+	context.subscriptions.push(dHello);
 
-	context.subscriptions.push(disposable);
+	let serverPath = "/Users/benl/devel/salt-vscode/bin/salt.sh";
+	let serverOptions: vsclient.ServerOptions = {
+		run:   { command: serverPath, args: ['-lsp'] },
+		debug: { command: serverPath, args: ['-lsp-debug']}
+	};
+
+	let clientOptions: vsclient.LanguageClientOptions = {
+		documentSelector: ['salt'],
+		synchronize: {
+			configurationSection: 'salt',
+			fileEvents: vscode.workspace.createFileSystemWatcher('**/.saltrc')
+		}
+	};
+
+	let dClient = new vsclient.LanguageClient(
+		'salt',
+		'Salt Language Server',
+		serverOptions,
+		clientOptions).start();
+	context.subscriptions.push(dClient);
+
+	console.log('Salt extension activated');
 }
 
 // this method is called when your extension is deactivated
